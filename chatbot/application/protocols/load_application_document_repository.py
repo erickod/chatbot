d5aba770ae3577dd8437827dcd2714b9c4c5bc3c
Erick Duarte <erickod@gmail.com>
2026-04-11 17:01:49 -0300

    refactor: Rewrite load_application_document_repository to use Protocol

--- chatbot/application/protocols/load_application_document_repository.py
+++ chatbot/application/protocols/load_application_document_repository.py
@@ -1,9 +1,10 @@
+from typing import Protocol
 from uuid import UUID
 
 from chatbot.domain.entities.application_document import ApplicationDocument
 
 
-class DocumentRepository:
+class DocumentRepository(Protocol):
     async def get_by_application_id(
         self, application_id: UUID
     ) -> ApplicationDocument | None: ...
