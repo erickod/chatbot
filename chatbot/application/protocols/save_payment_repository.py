2c1e29603a5c219f3a77ffd0c0ac232b2b90db2e
Erick Duarte <erickod@gmail.com>
2026-04-11 19:26:34 -0300

    feat(chatbot): add load_by_provider_id method to FakePaymentRepository and update SavePaymentRepository

--- chatbot/application/protocols/save_payment_repository.py
+++ chatbot/application/protocols/save_payment_repository.py
@@ -5,3 +5,6 @@ from chatbot.domain.entities.payment import Payment
 
 class PaymentRepository(Protocol):
     async def create(self, payment: Payment) -> None: ...
+    async def load_by_provider_id(
+        self, provider: str, provider_id: str
+    ) -> Payment | None: ...
--- chatbot/infra/repositories/fake_payment_repository.py
+++ chatbot/infra/repositories/fake_payment_repository.py
@@ -6,11 +6,18 @@ from chatbot.domain.entities.payment import Payment
 class FakePaymentRepository:
     def __init__(self, seed: list[Payment] = []) -> None:
         self.by_id: dict[UUID, Payment] = {}
+        self.by_provider: dict[tuple[str | None, str | None], Payment] = {}
         self._process_seed(seed)
 
     def _process_seed(self, seed: list[Payment]) -> None:
         for item in seed:
             self.by_id[item.id] = item
+            self.by_provider[item.provider, item.provider_id] = item
 
     async def create(self, payment: Payment) -> None:
         self._process_seed([payment])
+
+    async def load_by_provider_id(
+        self, provider: str, provider_id: str
+    ) -> Payment | None:
+        return self.by_provider.get((provider, provider_id), None)
