5800f41f3e7a6de886eb60cd72990f3d7b45b8da
Erick Duarte <erickod@gmail.com>
2026-04-11 15:40:46 -0300

    feat: implement save company repository protocol

--- chatbot/application/protocols/save_company_repository.py
+++ chatbot/application/protocols/save_company_repository.py
@@ -1,5 +1,7 @@
+from typing import Protocol
+
 from chatbot.domain.entities.customer import Customer
 
 
-class CompanyRepository:
+class CompanyRepository(Protocol):
     async def create(self, customer: Customer) -> None: ...
