fb3f10b2e27a9a2c51d57b4e0b806e942366171b
Erick Duarte <erickod@gmail.com>
2026-04-11 18:43:14 -0300

    feat: add FakePaymentGateway for use in tests

---
+++ chatbot/infra/external_services/fake_payment_gateway.py
@@ -0,0 +1,16 @@
+from uuid import uuid7
+
+from chatbot.domain.entities.payment import Payment
+
+
+class FakePaymentGateway:
+    def __init__(self) -> None: ...
+    async def create_charge(self, payment: Payment) -> None:
+        payment.register_gateway_reference(
+            gateway=self.__class__.__name__,
+            ref=uuid7().hex,
+            transaction_id="transaction_id",
+            qr_code_text="qr_code_text",
+            qr_code_uri="qr_code_uri",
+            expires_at=None,
+        )
