8a3531e09a6772732fe823ad19d0f4877e5dbacf
Erick Duarte <erickod@gmail.com>
2026-04-12 15:44:10 -0300

    fix: Improve thread safety in FastAPI server lifespan

--- chatbot/infra/http/server/fastapi/main.py
+++ chatbot/infra/http/server/fastapi/main.py
@@ -3,12 +3,20 @@ from typing import AsyncGenerator
 
 from fastapi import APIRouter, FastAPI
 
+from chatbot.infra.db.postgres import (
+    create_thread_safe_context,
+    teardown_thread_safe_context,
+)
 from chatbot.infra.http.routes.bot.resources import router as bot_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    yield
+    create_thread_safe_context(is_single_threaded=True)
+    try:
+        yield
+    finally:
+        await teardown_thread_safe_context()
 
 
 def configure_routes(application: FastAPI) -> None:
